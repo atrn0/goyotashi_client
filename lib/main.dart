cd8e6328414f1aac88fce7338617f1f7b2bf72fe
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-03-11 00:44:51 +0900

    refactor home_page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:goyotashi_client/pages/home_page.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,41 +9,50 @@ class HomePage extends StatelessWidget {
       body: Text('Hello World!'),
     );
   }
-}
 
-Widget _appBar(BuildContext context) {
-  return AppBar(
-    backgroundColor: Theme.of(context).primaryColor,
-    centerTitle: true,
-    title: Text(
-      'goyotashi',
-      style: TextStyle(
-        color: Colors.black,
+  AppBar _appBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).primaryColor,
+      centerTitle: true,
+      title: Text(
+        'goyotashi',
+        style: TextStyle(
+          color: Colors.black,
+        ),
       ),
-    ),
-    actions: <Widget>[
-      _profileButton(context),
-    ],
-  );
-}
+      actions: <Widget>[
+        _profileButton(),
+      ],
+    );
+  }
 
-Widget _profileButton(BuildContext context) {
-  final double _size = 36;
+  FlatButton _profileButton() {
+    final double _size = 36;
 
-  return FlatButton(
-    onPressed: null, // TODO: implements this
-//    child: ClipOval(
-//      child:
-//      ),
-//    ),
-    child: Container(
-      height: _size,
-      width: _size,
+    return FlatButton(
+      onPressed: null, // TODO: implements this
+      child: _circledImage(
+          image: AssetImage(
+            'assets/images/home_icon_mock.jpg', // TODO: replace with real image
+          ),
+          size: _size),
+    );
+  }
+
+  Container _circledImage({
+    @required ImageProvider image,
+    double size = 36,
+  }) {
+    return Container(
+      height: size,
+      width: size,
       decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage('assets/images/home_icon_mock.jpg'))),
-    ),
-  );
+        shape: BoxShape.circle,
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: image,
+        ),
+      ),
+    );
+  }
 }
