d05af0b8d7e8157738deeecb97432a437c336ac1
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-04-01 17:47:12 +0900

    sticky header

--- lib/pages/restaurants_page.dart
+++ lib/pages/restaurants_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goyotashi_client/components/restaurants/header.dart';
 import 'package:goyotashi_client/components/restaurants/restaurants_column.dart';
+import 'package:sticky_headers/sticky_headers.dart';
 
 class RestaurantsPage extends StatelessWidget {
   @override
@@ -21,19 +22,48 @@ class RestaurantsPage extends StatelessWidget {
             height: MediaQuery.of(context).size.height,
             color: Color(0xaaC4C4C4),
           ),
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40),
-              topRight: Radius.circular(40),
-            ),
-            child: Container(
-              padding: EdgeInsets.all(30),
-              height: double.infinity,
-              width: double.infinity,
-              color: Colors.white,
-              child: Text('hoge'),
-            )
-          )
+          ListView(
+            children: <Widget>[
+              SizedBox(height: 50),
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+                child: Container(height: 40, color: Colors.white),
+              ),
+              StickyHeader(
+                header: Container(
+                  padding: EdgeInsets.all(30),
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Text('hoge'),
+                ),
+                content: ConstrainedBox(
+                  constraints: BoxConstraints(
+                      minHeight: MediaQuery.of(context).size.height - 50),
+                  child: Container(
+                    color: Colors.white,
+                    width: double.infinity,
+                    child: Column(
+                      children: <Widget>[
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                        Text('restaurant1'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
