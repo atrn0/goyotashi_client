391061880a335e5dbfa824aca18bd49a3e1d97c2
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-04-01 18:51:10 +0900

    refactor restaurant_search_result.dart

--- lib/components/restaurants/restaurant_search_result.dart
+++ lib/components/restaurants/restaurant_search_result.dart
@@ -12,41 +12,49 @@ class RestaurantSearchResult extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Image(
-                width: _size,
-                height: _size,
-                fit: BoxFit.cover,
-                image: AssetImage(
-                  'assets/hiraganakan.jpg', // TODO: replace with real image
-                ),
-              ),
-              SizedBox(width: 8),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'キッチンゴリラ',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text('洋食')
-                ],
-              ),
-            ],
+          _restaurantInfo(),
+          _addButton(),
+        ],
+      ),
+    );
+  }
+
+  _restaurantInfo() {
+    return Row(
+      children: <Widget>[
+        Image(
+          width: _size,
+          height: _size,
+          fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/hiraganakan.jpg', // TODO: replace with real image
           ),
-          FlatButton(
-            onPressed: () {},
-            child: Column(
-              children: <Widget>[
-                Icon(Icons.add),
-                Text(
-                  'CAMPHOR-\nに追加',
-                  textAlign: TextAlign.center,
-                ),
-              ],
+        ),
+        SizedBox(width: 8),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'キッチンゴリラ',
+              style: TextStyle(fontSize: 20),
             ),
-          )
+            Text('洋食')
+          ],
+        ),
+      ],
+    );
+  }
+
+  _addButton() {
+    return FlatButton(
+      onPressed: () {},
+      child: Column(
+        children: <Widget>[
+          Icon(Icons.add),
+          Text(
+            'CAMPHOR-\nに追加',
+            textAlign: TextAlign.center,
+          ),
         ],
       ),
     );
