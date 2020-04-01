ce3e71087c57a761961e02bd4d6b5c94a2140cd2
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-04-01 18:49:20 +0900

    レストランの検索結果を実装

--- lib/components/restaurants/restaurant_search_result.dart
+++ lib/components/restaurants/restaurant_search_result.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RestaurantSearchResult extends StatelessWidget {
   final _size = 72.0;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 8),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Image(
-            width: _size,
-            height: _size,
-            fit: BoxFit.cover,
-            image: AssetImage(
-              'assets/hiraganakan.jpg', // TODO: replace with real image
-            ),
+          Row(
+            children: <Widget>[
+              Image(
+                width: _size,
+                height: _size,
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/hiraganakan.jpg', // TODO: replace with real image
+                ),
+              ),
+              SizedBox(width: 8),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'キッチンゴリラ',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text('洋食')
+                ],
+              ),
+            ],
           ),
+          FlatButton(
+            onPressed: () {},
+            child: Column(
+              children: <Widget>[
+                Icon(Icons.add),
+                Text(
+                  'CAMPHOR-\nに追加',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
