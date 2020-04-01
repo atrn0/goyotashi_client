5e81ba209662f9d3c691e8adcd3f758670f5e3d3
atrn0 <14242642+aratasato@users.noreply.github.com>
2020-04-01 17:58:03 +0900

    refactor search_restaurant_sheet.dart

--- lib/components/restaurants/search_restaurant_sheet.dart
+++ lib/components/restaurants/search_restaurant_sheet.dart
@@ -6,11 +6,7 @@ class SearchRestaurantSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: <Widget>[
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Color(0xaaC4C4C4),
-        ),
+        _background(context),
         ListView(
           children: <Widget>[
             SizedBox(height: 50),
@@ -22,37 +18,57 @@ class SearchRestaurantSheet extends StatelessWidget {
               child: Container(height: 40, color: Colors.white),
             ),
             StickyHeader(
-              header: Container(
-                padding: EdgeInsets.all(30),
-                width: double.infinity,
-                color: Colors.white,
-                child: Text('hoge'),
-              ),
-              content: ConstrainedBox(
-                constraints: BoxConstraints(
-                    minHeight: MediaQuery.of(context).size.height - 50),
-                child: Container(
-                  color: Colors.white,
-                  width: double.infinity,
-                  child: Column(
-                    children: <Widget>[
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                      Text('restaurant1'),
-                    ],
-                  ),
-                ),
-              ),
+              header: _header(),
+              content: _content(context),
             ),
           ],
         ),
+      ],
+    );
+  }
+
+  _background(context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      color: Color(0xaaC4C4C4),
+    );
+  }
+
+  _header() {
+    return Container(
+      padding: EdgeInsets.all(30),
+      width: double.infinity,
+      color: Colors.white,
+      child: Text('hoge'),
+    );
+  }
+
+  _content(context) {
+    return ConstrainedBox(
+      constraints:
+          BoxConstraints(minHeight: MediaQuery.of(context).size.height - 50),
+      child: Container(
+        color: Colors.white,
+        width: double.infinity,
+        child: _searchResults(),
+      ),
+    );
+  }
+
+  _searchResults() {
+    return Column(
+      children: <Widget>[
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
+        Text('restaurant1'),
       ],
     );
   }
